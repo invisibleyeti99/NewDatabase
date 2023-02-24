42124e4869c187c1547facfe1e01bb7ce1c96d9d
Matthew Davies <mattjdavies199@gmail.com>
2023-02-23 21:02:32 -0700

    Add project files.

---
+++ NewDatabase/Models/ApplicationResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mdavies9_mission6.Models
+{
+    public class ApplicationResponse
+    {
+
+
+        [Required]
+        [Key]
+        public int TaskId { get; set; }
+        [Required]
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        public string Task { get; set; }
+
+        public string DueDate { get; set; }
+        [Required]
+        public string Quadrant
+        { get; set; }
+
+        public bool Completed { get; set; }
+
+
+    }
+}
